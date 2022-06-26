edc172d1f945b362a6be1e20506b210ad6cc65aa
KaterinaDavy <katerinadavy@gmail.com>
2022-06-26 19:24:45 +0300

    Task1

--- src/lesson_2/Task_1.java
+++ src/lesson_2/Task_1.java
@@ -6,20 +6,20 @@ import java.util.Scanner;
 public class Task_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите ваше полное имя");
+        System.out.println("Р’РІРµРґРёС‚Рµ РІР°С€Рµ РїРѕР»РЅРѕРµ РёРјСЏ");
         String name = scanner.nextLine();
         System.out.println(isMale(name));
-        System.out.println("Введите свой возраст");
+        System.out.println("Р’РІРµРґРёС‚Рµ СЃРІРѕР№ РІРѕР·СЂР°СЃС‚");
         int age = scanner.nextInt();
         System.out.println(isAges(age));
     }
     private static String isMale(String name) {
-        String[] letters = {"а", "у", "е", "о", "э", "я", "и", "ю"};
+        String[] letters = {"Р°", "Сѓ", "Рµ", "Рѕ", "СЌ", "СЏ", "Рё", "СЋ"};
         String letter = name.substring(name.length() - 1);
         if (Arrays.asList(letters).contains(letter)) {
-            return "Приветствую,самая обоятельная и привлекательная";
+            return "РџСЂРёРІРµС‚СЃС‚РІСѓСЋ,СЃР°РјР°СЏ РѕР±РѕСЏС‚РµР»СЊРЅР°СЏ Рё РїСЂРёРІР»РµРєР°С‚РµР»СЊРЅР°СЏ";
         } else {
-            return "Здравствуй, друг, товарищ, брат";
+            return "Р—РґСЂР°РІСЃС‚РІСѓР№, РґСЂСѓРі, С‚РѕРІР°СЂРёС‰, Р±СЂР°С‚";
         }
 
     }
@@ -27,10 +27,10 @@ public class Task_1 {
     private static String isAges(int age) {
         switch (age) {
             case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 -> {
-                return "Поздравляю, у вас вся жизнь впереди";
+                return "РџРѕР·РґСЂР°РІР»СЏСЋ, Сѓ РІР°СЃ РІСЃСЏ Р¶РёР·РЅСЊ РІРїРµСЂРµРґРё";
             }
             default -> {
-                return "Не расстраиваетесь, возраст -это всего лишь цифры";
+                return "РќРµ СЂР°СЃСЃС‚СЂР°РёРІР°РµС‚РµСЃСЊ, РІРѕР·СЂР°СЃС‚ -СЌС‚Рѕ РІСЃРµРіРѕ Р»РёС€СЊ С†РёС„СЂС‹";
             }
         }
     }
