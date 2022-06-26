616fe1611bd28485a602ca66db3e96fd12b8a7b3
KaterinaDavy <katerinadavy@gmail.com>
2022-06-26 18:08:53 +0300

    Task1

--- src/lesson_2/Task_1.java
+++ src/lesson_2/Task_1.java
@@ -1,4 +1,32 @@
 package lesson_2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваше полное имя");
+        String name = scanner.nextLine();
+        System.out.println(isMale(name));
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Введите свой возраст");
+        int age = scanner1.nextInt();
+        System.out.println(isAges(age));
+    }
+    private static String isMale(String name) {
+        String[] letters = {"а", "у", "е", "о", "э", "я", "и", "ю"};
+        String letter = name.substring(name.length() - 1);
+        if (Arrays.asList(letters).contains(letter)) {
+            return "Приветствую,самая обоятельная и привлекательная";
+        } else {
+            return "Здравствуй, друг, товарищ, брат";
+        }
+
+
+    }
+
+    private static String isAges(String age) {
+        String[] numbers = {}
+    }
 }
