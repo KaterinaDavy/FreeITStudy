b5dfb1da881effced8fdbe6d0d816fe84088b2fe
KaterinaDavy <katerinadavy@gmail.com>
2022-06-27 20:25:12 +0300

    Task1

--- src/lesson_2/Task_1.java
+++ src/lesson_2/Task_1.java
@@ -11,7 +11,11 @@ public class Task_1 {
         System.out.println(isMale(name));
         System.out.println("Введите свой возраст");
         int age = scanner.nextInt();
-        System.out.println(isAges(age));
+        if ( age <= 18) {
+            System.out.println("Поздравляю, у вас вся жизнь впереди");
+        } else {
+            System.out.println("Не печальтесь, возраст-это всего лищь цифры");
+        };
     }
     private static String isMale(String name) {
         String[] letters = {"а", "у", "е", "о", "э", "я", "и", "ю"};
@@ -24,14 +28,7 @@ public class Task_1 {
 
     }
 
-    private static String isAges(int age) {
-        switch (age) {
-            case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 -> {
-                return "Поздравляю, у вас вся жизнь впереди";
-            }
-            default -> {
-                return "Не расстраиваетесь, возраст -это всего лишь цифры";
-            }
-        }
+
     }
-}
+
+
