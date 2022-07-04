a348c0d823c5cda5ddd2ddd03d17328706c816f6
KaterinaDavy <katerinadavy@gmail.com>
2022-07-04 21:46:39 +0300

    lesson2 task2-Strings

---
+++ src/Game.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Random;
+
+//public class Game {
+//    public static void main(String[] args) {
+//
+//        Random letter = new Random();
+//        char computerLetter = (char) (letter.nextInt(26) + 'a');
+////        int computerLetter = letter.nextInt(26);
+//        System.out.println("Enter a letter");
+//
+//
+//
+//        Scanner scanner = new Scanner(System.in);
+//        while (true){
+//            String myAnswer = letter.nextLine();
+//            char myLetter = myAnswer.charAt(0);
+//            int codeLetter = myLetter;
+//
+////        while (true) {
+////            c = scanner.nextInt();
+////            if(letter == c) {
+////                System.out.println("Yes");
+////                break;
+////        for (int i =0; (char) System.in.read() != )
+//
+//            }
+//        }
+//
+//
+//////        char random = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+////        char randomLetter = (char) ('a' + Math.random() * ('z'-'a' + 1));
+//
+//
+//
+//
+//    }
+//}
---
+++ src/less.java
@@ -0,0 +1,18 @@
+import jdk.jfr.Frequency;
+
+public class less {
+    public static void main(String[] args) {
+        String[] sentence = {"I", "love", "you", "I", "love", "you", "Java"};
+        int word = 1;
+        int word1 = 2;
+        int word2 = 3;
+        int word3 = 4;
+        for(int i=0; i < sentence.length;  i++) {
+            System.out.println(word);
+           }
+        for(int i=1; i < sentence.length;  i++) {
+            System.out.println(word1);
+
+
+        }
+    }
