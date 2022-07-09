9e5d88d1ed0a2a6796b2058c0995c7d4bb633c11
KaterinaDavy <katerinadavy@gmail.com>
2022-07-09 23:49:49 +0300

    Guess the letter

--- src/Game.java
+++ src/Game.java
@@ -6,48 +6,24 @@ public class Game {
 
         Random letter = new Random();
         char computerLetter = (char) (letter.nextInt(26) + 'a');
-//        int computerLetter = letter.nextInt(26);
-//        char ch=
-//        System.out.println(computerLetter);
-//        System.out.println("Enter a letter");
-////
-////
-////
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a letter");
-        String name = scanner.nextLine();
-//        boolean exit = false;
-//        while (!exit) {
-//
-//            int scannerLetter = scanner.nextLine().charAt(0);
-//
-//            if (computerLetter == scannerLetter) {
-//                System.out.println("Win");
-//            } else if (computerLetter-scannerLetter < 5)
-//                System.out.println("Hot");
-//        }
 
-//        while (true){
-//            String myAnswer = letter.nextLine();
-//            char myLetter = myAnswer.charAt(0);
-//            int codeLetter = myLetter;
-//
-////        while (true) {
-////            c = scanner.nextInt();
-////            if(letter == c) {
-////                System.out.println("Yes");
-////                break;
-////        for (int i =0; (char) System.in.read() != )
-//
-//            }
-//        }
-//
-//
-//////        char random = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-////        char randomLetter = (char) ('a' + Math.random() * ('z'-'a' + 1));
-//
-//
-//
-//
+        while (true) {
+
+            int scannerLetter = scanner.nextLine().charAt(0);
+
+            if (computerLetter == scannerLetter) {
+                System.out.println("Win");
+                break;
+            } else if (computerLetter-scannerLetter < 5) {
+                System.out.println("Hot");
+            } else if (computerLetter-scannerLetter > 5) {
+                System.out.println("Cold");
+            }
+
+        }
+
     }
 }
