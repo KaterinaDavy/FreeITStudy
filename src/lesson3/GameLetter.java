1c4ee9cd1ca5c2964e593efe4e4b8f48b3d138b6
KaterinaDavy <katerinadavy@gmail.com>
2022-07-10 00:25:37 +0300

    Guess the letter

---
+++ src/lesson3/GameLetter.java
@@ -0,0 +1,27 @@
+package lesson3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameLetter {
+    public static void main(String[] args) {
+        Random letter = new Random();
+        char computerLetter = (char) (letter.nextInt(26) + 'a');
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a letter");
+
+        while (true) {
+            int scannerLetter = scanner.nextLine().charAt(0);
+            if (computerLetter == scannerLetter) {
+                System.out.println("You won");
+                break;
+            } else if (computerLetter-scannerLetter < 5) {
+                System.out.println("Hot");
+            } else if (computerLetter-scannerLetter >5) {
+                System.out.println("Cold");
+            }
+        }
+    }
+
+}
